de3795e72a5b437f822f0e85b433511f80094c06
Zerosky00 <dayanapenagosa@gmail.com>
2021-11-02 20:39:31 -0500

    Actualización3 - Fondos - Registro - Inicio de sesion

---
+++ app/src/main/java/com/example/signtoall/InicioSesionMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.signtoall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioSesionMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion_main);
+    }
+}
