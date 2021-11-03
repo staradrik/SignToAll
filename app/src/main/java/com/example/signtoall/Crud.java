3e244f40f3c74ba9de97b99579374502aa928c5a
Niyi88 <lorenacortesb31@hotmail.com>
2021-11-03 14:55:00 -0500

    Actualización 6: se agrego la activity para poder visualizar el listado de estudiantes registrados por parte del docente, se colocaron los respectivos componentes que permiten hacer el crud y finalmente la seleccion del curso.

--- app/src/main/java/com/example/signtoall/Crud.java
+++ app/src/main/java/com/example/signtoall/Crud.java
@@ -2,7 +2,9 @@ package com.example.signtoall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Crud extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Crud extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crud);
+    }
+    public void irlistado(View view){
+        Intent i=new Intent(this, Listado.class);
+
+        startActivity(i);
     }
 }
