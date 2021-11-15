46f6fe11394b89303e356d85894334786894dc63
Niyi88 <lorenacortesb31@hotmail.com>
2021-11-15 16:49:17 -0500

    Se agregaron dos activities para cada asigantura y se agregaron los dos botones para el registro de los dos roles del sistema.

--- app/src/main/java/com/example/signtoall/Seleccion.java
+++ app/src/main/java/com/example/signtoall/Seleccion.java
@@ -2,7 +2,9 @@ package com.example.signtoall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Seleccion extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Seleccion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion);
+    }
+    public void matema (View view){
+        Intent i=new Intent(this, matematicas.class);
+
+        startActivity(i);
+    }
+    public void espanol (View view){
+        Intent i=new Intent(this, espanol.class);
+        startActivity(i);
     }
 }
